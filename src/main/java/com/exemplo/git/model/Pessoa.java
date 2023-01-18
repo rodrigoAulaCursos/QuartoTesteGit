4b82e9e8b620a194afce2087e171af69bbc35257
Rodrigo Fernandes <rodrigofernandes3@hotmail.com>
2023-01-18 09:22:59 -0300

    Adicionado o encapsulamento dos atributos e alterado o atributo nome para nomePessoa

--- src/main/java/com/exemplo/git/model/Pessoa.java
+++ src/main/java/com/exemplo/git/model/Pessoa.java
@@ -2,8 +2,29 @@ package com.exemplo.git.model;
 
 public class Pessoa {
 	
-	private String nome;
+	private String nomePessoa;
 	private String email;
 	private String cpf;
+	
+	public String getNomePessoa() {
+		return nomePessoa;
+	}
+	public void setNomePessoa(String nomePessoa) {
+		this.nomePessoa = nomePessoa;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
 
 }
